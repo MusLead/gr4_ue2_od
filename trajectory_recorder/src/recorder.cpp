a80bc89c1cde52665f98231a358a19cc45b0360b
RoboLab <robolab@hs-fulda.de>
2025-05-17 15:14:54 +0200

    feat: add IMU subscriber

--- trajectory_recorder/src/recorder.cpp
+++ trajectory_recorder/src/recorder.cpp
@@ -14,12 +14,36 @@ public:
     TrajectoryRecorder(std::ofstream& odom_out, std::ofstream& imu_out) : 
         Node("trajectory_recorder"), odom_out_(odom_out), imu_out_(imu_out)
     {
-        subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        subscription_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
             "rosbot_base_controller/odom", 10,
             [&](const nav_msgs::msg::Odometry& msg){
-                std::cout << "I heard: x " << msg.pose.pose.position.x << std::endl;
+                std::cout << "[Odom] x " << msg.pose.pose.position.x 
+                    << ", y "<< msg.pose.pose.position.y 
+                    << std::endl;
+                odom_out_ << msg.pose.pose.position.x << " "
+                          << msg.pose.pose.position.y  << std::endl;
+                odom_out_.flush();
             }
-            );
+        );
+
+        subscription_imu_ = this->create_subscription<sensor_msgs::msg::Imu>(
+            "/imu_broadcaster/imu", 10,
+            [&](const sensor_msgs::msg::Imu& msg){
+                std::cout << "[IMU] time: " << msg.header.stamp.nanosec << std::endl;
+                imu_out_ << msg.orientation.x << " "
+                         << msg.orientation.y << " "
+                         << msg.orientation.z << " "
+                         << msg.orientation.w << " "
+                         << msg.angular_velocity.x << " "
+                         << msg.angular_velocity.y << " "
+                         << msg.angular_velocity.z << " "
+                         << msg.linear_acceleration.x << " "
+                         << msg.linear_acceleration.y << " "
+                         << msg.linear_acceleration.z << std::endl;
+                imu_out_.flush();
+            }
+        );
+  
     }
 
 
@@ -31,7 +55,8 @@ private:
     std::ofstream& odom_out_;
     std::ofstream& imu_out_;
 
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_odom_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_imu_;
 
 };
 
