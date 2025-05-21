17054a187543d20b5366df718acafd8c9314c14c
RoboLab <robolab@hs-fulda.de>
2025-05-21 10:25:52 +0200

    refactor: add debug output

--- trajectory_recorder/src/recorder.cpp
+++ trajectory_recorder/src/recorder.cpp
@@ -16,8 +16,8 @@ using std::cos;
 class TrajectoryRecorder : public rclcpp::Node
 {
 public:
-    TrajectoryRecorder(std::ofstream& odom_out, std::ofstream& imu_out) : 
-        Node("trajectory_recorder"), odom_out_(odom_out), imu_out_(imu_out)
+    TrajectoryRecorder(std::ofstream& odom_out, std::ofstream& imu_out, std::ofstream& imu_debug_out) : 
+        Node("trajectory_recorder"), odom_out_(odom_out), imu_out_(imu_out), imu_debug_out_(imu_debug_out)
     {
         subscription_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
             "rosbot_base_controller/odom", 10,
@@ -34,14 +34,26 @@ public:
         subscription_imu_ = this->create_subscription<sensor_msgs::msg::Imu>(
             "/imu_broadcaster/imu", 10,
             [&](const sensor_msgs::msg::Imu& msg){
+
              	double t_now = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9;
 				if (t_vor == 0) {
-					t_vor = t_now;
-				} else {
-					double ax = msg.linear_acceleration.x;
-					double ay = msg.linear_acceleration.y;
-					double az = msg.linear_acceleration.z;	
-					double omega = msg.angular_velocity.z;
+                    
+                    ax = msg.linear_acceleration.x;
+					ay = msg.linear_acceleration.y;
+					az = msg.linear_acceleration.z;
+                    omega = msg.angular_velocity.z;
+
+				} else if(ax != msg.linear_acceleration.x || 
+                    ay != msg.linear_acceleration.y || 
+                    az != msg.linear_acceleration.z ||
+                    omega != msg.angular_velocity.z) {
+
+                    log_imu_debug(msg);
+					
+                    ax = msg.linear_acceleration.x;
+					ay = msg.linear_acceleration.y;
+					az = msg.linear_acceleration.z;	
+					omega = msg.angular_velocity.z;
 
 					double delta_t = t_now - t_vor;
 					double v = sqrt(pow(ax * delta_t, 2.0) + pow(ay * delta_t, 2.0) + pow(az * delta_t, 2.0));
@@ -57,6 +69,7 @@ public:
 					y_vor = yn;
 					rotateAngular_vor = rotateAngular;
 				}
+                t_vor = t_now;
             }
         );
   
@@ -64,16 +77,39 @@ public:
 
 
 private:
+    void log_imu_debug(const sensor_msgs::msg::Imu& msg)
+    {
+        imu_debug_out_ << "[IMU DEBUG] Orientation: ("
+               << msg.orientation.x << ", "
+               << msg.orientation.y << ", "
+               << msg.orientation.z << ", "
+               << msg.orientation.w << "), "
+               << "Angular Velocity: ("
+               << msg.angular_velocity.x << ", "
+               << msg.angular_velocity.y << ", "
+               << msg.angular_velocity.z << "), "
+               << "Linear Acceleration: ("
+               << msg.linear_acceleration.x << ", "
+               << msg.linear_acceleration.y << ", "
+               << msg.linear_acceleration.z << ")"
+               << std::endl;
+
+        imu_debug_out_.flush();
+    }
 
     // You can use this streams to output
     // the coordinates of your computed
     // poses
     std::ofstream& odom_out_;
     std::ofstream& imu_out_;
+    std::ofstream& imu_debug_out_;
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_odom_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_imu_;
 	
+    bool isMoving = false;
+    double x_odom = 0, y_odom = 0;
+    double ax, ay, az, omega;
 	double t_vor = 0;
 	double x_vor = 0, y_vor = 0;
 	double rotateAngular_vor = 0;
@@ -83,9 +119,10 @@ int main(int argc, char** argv)
 {
     std::ofstream odom_out("odometry.txt", std::ios::out | std::ios::trunc);
     std::ofstream imu_out("imu.txt", std::ios::out | std::ios::trunc);
+    std::ofstream imu_debug_out_("imu_debug.txt", std::ios::out | std::ios::trunc);
 
 
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TrajectoryRecorder>(odom_out, imu_out));
+    rclcpp::spin(std::make_shared<TrajectoryRecorder>(odom_out, imu_out, imu_debug_out_));
     rclcpp::shutdown();
 }
